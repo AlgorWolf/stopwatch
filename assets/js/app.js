04f75365ffa7570cee9f898bc79fdb0bb8ef88bb
Doubfire <eedegware@gmail.commkdir foolworkcd foolworkclear$ git config --global user.email eedegware@gmail.com>
2022-06-11 22:49:24 +0100

    first release of stopwatch

---
+++ assets/js/app.js
@@ -0,0 +1,4 @@
+const stopwatchDisplay = document.querySelector('.stopwatch-display')
+const stopwatchStartBtn = document.querySelector('.btn-stopwatch-start')
+const stopwatchStopBtn = document.querySelector('.btn-stopwatch-stop')
+const stopwatchresetBtn = document.querySelector('.btn-stopwatch-reset')
